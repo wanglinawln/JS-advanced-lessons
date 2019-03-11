5017f31a52bf64789f42f7313cc456a83a9d1aec
wanglinawln <1416978302@qq.com>
2019-03-11 13:35:51 +0800

    add

---
+++ 课程练习/LS06/js/demo14.js
@@ -0,0 +1,13 @@
+//逻辑与、或的基本理解
+console.log(20&&0);
+console.log(2&&4);
+console.log(null&&10);
+console.log(2>1&&4<5);
+console.log(true&&(!2));
+console.log(false&&("2" == 2));
+console.log(false&&false);
+
+console.log(2>1||4<5);
+console.log(true||(!2));
+console.log(false||("2" == 2));
+console.log(false||false);
