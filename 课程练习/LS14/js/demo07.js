746c07500308dfb3decdef733a67410b7383ee57
wanglinawln <1416978302@qq.com>
2019-04-15 17:20:44 +0800

    add

---
+++ 课程练习/LS14/js/demo07.js
@@ -0,0 +1,13 @@
+//修改obj属性的特性
+var obj = {
+    x:1,
+    y:2
+};
+//Object.defineProperty(obj,"x",{writable:false,value:11,enumerable:false,configurable:true});
+Object.defineProperty(obj,"x",{enumerable:false});
+for(var k in obj){
+    console.log(k,obj[k]);
+}
+
+//思考：如果只更改writable特性的话，enumerable特性是否被修改
+
